aeb145bb77196a718b9f7731c3d06264f01459b5
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2021-07-31 18:39:30 +0600

    hobe

--- comment.php
+++ comment.php
@@ -3,27 +3,24 @@
 # connect to the server
 include "_dbConnection.php";
 
-if ($conn == false) {
+if ($conn === false) {
     echo 'Data base did not connected';
 }
 $sw = "SELECT * FROM `contact` ORDER BY `contact`.`time` DESC";
 $r1 = mysqli_query($conn, $sw);
 $c1 = mysqli_num_rows($r1);
-
+$count = 3;
 if ($c1 > 0) {
-    while ($row = mysqli_fetch_row($r1)) {
+    while (($row = mysqli_fetch_row($r1)) && ($count--)) {
 ?>
-
-
         <div class="form-group">
             <label for="name"> নামঃ <?php echo "{$row[0]}"; ?></label>
         </div>
-        <label for="message"> কমেন্টঃ <?php echo "{$row[4]}"; ?></label>
+        <div class="form-group">
+            <label for="message"> কমেন্টঃ <?php echo "{$row[4]}"; ?></label>
         </div>
         <br>
 <?php
-
-        break;
     }
 } else {
     echo 'There have no link in database';
--- saazidgeneralstore.sql
+++ saazidgeneralstore.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2021 at 09:56 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Generation Time: Jul 31, 2021 at 02:22 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,16 +32,19 @@ CREATE TABLE `contact` (
   `email` text NOT NULL,
   `phone` text NOT NULL,
   `address` text NOT NULL,
-  `comment` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `comment` text NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `contact`
 --
 
-INSERT INTO `contact` (`name`, `email`, `phone`, `address`, `comment`) VALUES
-('Nur', 'dimuna@gmail.com', '0192834859', 'Dimu na', 'Shiddho dim cai'),
-('Manik', 'md.nurealamsiddiquee2004@gmail.com', '0178485893872', 'aldkfjh', 'adslkfj');
+INSERT INTO `contact` (`name`, `email`, `phone`, `address`, `comment`, `time`) VALUES
+('Nur', 'manik@yahool.com', '029477199', 'skdfjal', 'Good', '2021-07-31 12:12:02'),
+('Manik', 'md.nurealamsiddiquee2004@gmail.com', '0178485893872', 'Jani na', 'Not bad', '2021-07-31 12:15:47'),
+('Rabbi', 'kha@gmail.com', '03847929', 'hmm', 'Okay', '2021-07-31 12:19:43'),
+('Tahmid', 'khale@gmail.com', '0193875891', 'hmm', 'Brilliant', '2021-07-31 12:22:06');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
